1930c69f4a58f3b58dae6c206b1b078fe44d1f65
ravikakadia <40834512+ravikakadia@users.noreply.github.com>
2023-09-05 18:42:17 +0530

    Update versioncompare.java

--- versioncompare.java
+++ versioncompare.java
@@ -1,4 +1,4 @@
-class Solution {
+class VersionCompare {
     public int compareVersion(String version1, String version2) {
      String[] v1 = version1.split("\\.");
     String[] v2 = version2.split("\\.");
