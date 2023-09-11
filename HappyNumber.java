cbef7415a2947f69d99de81ed973f0628cb9fbef
ravikakadia <40834512+ravikakadia@users.noreply.github.com>
2023-09-11 12:18:34 +0530

    Create HappyNumber.java
    adding new example

---
+++ HappyNumber.java
@@ -0,0 +1,33 @@
+class HappyNumber {
+  
+  public static int sumOfSquaredDigits(int number) {
+      int totalSum = 0;
+      while (number != 0) {
+        int digit = number % 10;
+        number = number / 10;
+        totalSum += (Math.pow(digit, 2));
+      }
+      return totalSum;
+  }
+  public static boolean isHappyNumber(int n) {
+      int slowPointer = n; 
+      int fastPointer = sumOfSquaredDigits(n); 
+
+      while (fastPointer != 1 && slowPointer != fastPointer) {
+          slowPointer = sumOfSquaredDigits(slowPointer);
+          fastPointer =  sumOfSquaredDigits(sumOfSquaredDigits(fastPointer));
+      }
+      return fastPointer == 1;
+  }
+
+  public static void main(String args[]) {
+    int a[] = {1, 5, 19, 25, 7};
+    for (int i = 0; i < a.length; i++) {
+      System.out.println((i + 1) + ".\tInput Number: " + a[i]);
+      String output = isHappyNumber(a[i]) ? "True" : "False";
+      
+      System.out.println("\n\tIs it a happy number? " + output);
+      System.out.println(new String(new char[100]).replace('\0', '-'));
+    }
+  }
+}
